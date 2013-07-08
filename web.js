7cc953b41662fa3a0b4b7342815f733d521ad2d0
Reed Mangino <reed@themanginos.com>
2013-07-08 15:59:42 +0000

    changed message

--- web.js
+++ web.js
@@ -3,7 +3,8 @@ var express = require('express');
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello Again!');
+  s = fs.readFileSync('./index.html');
+  response.send(s);
 });
 
 var port = process.env.PORT || 5000;
